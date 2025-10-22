4321bb12a729fc4942f986039e6bafbbecf7a5ef
lamaA <lamaAlbugami@outlook.com>
2025-10-22 22:15:33 +0300

    model1

--- db_connection.php
+++ db_connection.php
@@ -1,14 +1,13 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$servername = "127.0.0.1";  // استخدم 127.0.0.1 بدل localhost
+$servername = "localhost";
 $username   = "root";
-$password   = "root";       // كلمة المرور الافتراضية في MAMP على Windows وmacOS
+$password   = "root";
 $dbname     = "tanafs";
-$port       = 3306;
 
 try {
-    $conn = new mysqli($servername, $username, $password, $dbname, $port);
+    $conn = new mysqli($servername, $username, $password, $dbname);
     $conn->set_charset('utf8mb4');
 } catch (mysqli_sql_exception $e) {
     die("Database connection failed: " . $e->getMessage());
